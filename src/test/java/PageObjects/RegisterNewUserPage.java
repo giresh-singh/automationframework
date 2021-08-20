1fd1925410907c3be7d20eb12c26e6e218475499
Giresh Singh <gireshsingh@hotmail.com>
2021-08-20 20:29:20 +0530

    Prepared test scenarios RegisterNewUserPage and RegisterNewUserTest

--- src/test/java/PageObjects/RegisterNewUserPage.java
+++ src/test/java/PageObjects/RegisterNewUserPage.java
@@ -1,12 +1,15 @@
 package PageObjects;
 
 import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.ubs.dis.framework.selenium.WaitHelper;
 import org.ubs.dis.framework.utilities.LoggerHelper;
+
+import java.util.List;
 
 public class RegisterNewUserPage {
     /**
@@ -123,7 +126,7 @@ public class RegisterNewUserPage {
         PageFactory.initElements(driver,this);
         waitHelper = new WaitHelper(driver);
         waitHelper.waitForElement(lnkSignin,30);
-        log.info(String.format("RegisterPage initiated with all its elements"));
+        log.info(String.format("RegisterNewUserPage initiated with all its elements"));
     }
 
     public boolean clicOnSignIn() {
@@ -164,6 +167,219 @@ public class RegisterNewUserPage {
             log.error(ex.getMessage());
             return false;
         }
+    }
+
+    public boolean selectPrefix(String val){
+        String prefix = val.toLowerCase();
+        switch(prefix){
+            case "mr.":
+                radioPrefixMr.click();
+                return true;
+            case "mrs.":
+                radioPrefixMrs.click();
+                return true;
+            default:
+                ErrDescription = String.format("Option <b>%s</b> is not correct option",val);
+        }
+        return false;
+    }
+
+    public boolean fillPersonalInfo(String fName,String lName, String password){
+        boolean blnFlag = false;
+        try{
+            txtCustomerFName.sendKeys(fName);
+            Threadwait();
+            txtCustomerLName.sendKeys(lName);
+            Threadwait();
+            txtSetPassword.sendKeys(password);
+            Threadwait();
+            return blnFlag=true;
+        }catch(Exception ex){
+            ErrDescription = ex.getMessage();
+            log.error(ex.getMessage());
+            return blnFlag;
+        }
+    }
+
+    public boolean selectDOB(String strDays,String strMonth,String strYears){
+        boolean blnFlag = false;
+        blnFlag = Select(dropDOBDate,strDays,"byvalue");
+        if(!blnFlag){
+            ErrDescription = String.format("Not able to select Date %s from DOB drop down",strDays);
+            return blnFlag=false;
+        }
+        blnFlag = Select(dropDOBMonth,strMonth,"byvalue");
+        if(!blnFlag){
+            ErrDescription = String.format("Not able to select Month %s from DOB drop down",strMonth);
+            return blnFlag=false;
+        }
+        blnFlag = Select(dropDOBYear,strYears,"byvalue");
+        if(!blnFlag){
+            ErrDescription = String.format("Not able to select Year %s from DOB drop down",strYears);
+            return blnFlag=false;
+        }
+        return true;
+    }
+
+    public boolean fillYourAddress(String fName,String lName, String company,String address1,String strCity){
+        boolean blnFlag = false;
+        try{
+            txtFName.sendKeys(fName);
+            Threadwait();
+            txtLName.sendKeys(lName);
+            Threadwait();
+            txtCompany.sendKeys(company);
+            Threadwait();
+            txtAddressLine1.sendKeys(address1);
+            Threadwait();
+            txtAddressLine2.sendKeys(address1);
+            Threadwait();
+            txtCity.sendKeys(strCity);
+            Threadwait();
+            return blnFlag=true;
+        }catch(Exception ex){
+            ErrDescription = ex.getMessage();
+            log.error(ex.getMessage());
+            return blnFlag;
+        }
+    }
+
+    public boolean selectState(String strStateName){
+        return Select(dropState,"Florida","visibletext");
+    }
+
+    public boolean enterZipCode(String strZip){
+        try {
+            txtZipCode.sendKeys(strZip);
+            Threadwait();
+            return true;
+        }catch (Exception ex){
+            ErrDescription = ex.getMessage();
+            return  false;
+        }
+    }
+
+    public boolean fillMobileDetails(String strHomePhone, String strMobile,String strAliasAdd){
+        try {
+            txtHomePhone.sendKeys(strHomePhone);
+            Threadwait();
+            txtMobilePhone.sendKeys(strMobile);
+            Threadwait();
+            txtAliasAddress.sendKeys(strAliasAdd);
+            Threadwait();
+            return true;
+        }catch(Exception ex){
+            ErrDescription = ex.getMessage();
+            return false;
+        }
+    }
+
+    public boolean clickRegister(){
+        try {
+            btnRegister.click();
+            Threadwait(10);
+            return true;
+        }catch(Exception ex){
+            ErrDescription = ex.getMessage();
+            return false;
+        }
+    }
+
+    public boolean logOutApp(){
+        boolean blnFlag = false;
+        try{
+            //Click on Sign Out link
+            lnklogOut.click();
+            //Verify logged out
+            waitHelper.waitForElement(lblAuthentication, 30);
+            log.info(String.format("Logout is successful....."));
+            return blnFlag = true;
+        }catch(Exception ex){
+            log.error(String.format("Not able Logout from application getting error %s",ex.getMessage()));
+            return  blnFlag;
+        }
+    }
+
+    /***
+     * This function selects the value based on certain condition for 'Element(string strObjectName)'
+     * @param strListValue -: Value to be selected
+     * @param selectType -: Different Select Type (visibletext,byvalue,byindex,partialtext)
+     * @param element Element of select box
+     * @return True/false
+     */
+    public boolean Select(WebElement element,String strListValue,String selectType){
+        boolean flag = false;
+        try{
+            if (element == null){
+                return false;
+            }
+            try
+            {
+                switch (selectType.toLowerCase()) {
+                    case "visibletext":
+                        new org.openqa.selenium.support.ui.Select(element).selectByVisibleText(strListValue);
+                        return true;
+                    case "byvalue":
+                        new org.openqa.selenium.support.ui.Select(element).selectByValue(strListValue);
+                        return true;
+                    case "byindex":
+                        new org.openqa.selenium.support.ui.Select(element).selectByIndex(Integer.parseInt(strListValue));
+                        return true;
+                    case "partialtext":
+                        org.openqa.selenium.support.ui.Select select = new org.openqa.selenium.support.ui.Select(element);
+                        List<WebElement> lstOptions = select.getOptions();
+                        String strvalue="";
+                        for(WebElement option:lstOptions){
+                            if(option.getText().contains(strListValue)) {
+                                strvalue = option.getAttribute("value");
+                                break;
+                            }
+                        }
+                        if(!strvalue.equals("")){
+                            select.selectByValue(strvalue);
+                            return true;
+                        }else{
+                            ErrDescription = String.format("Option <b>%s</b> is not valid option",strvalue);
+                            return false;
+                        }
+                }
+
+            }
+            catch (Exception e)
+            {
+                element.click();
+                element.findElement(By.xpath("//*[text()='" + strListValue + "']"));
+            }
+
+            flag = true;
+        }
+        catch (Exception e)
+        {
+            flag = false;
+            ErrDescription = e.getMessage();
+        }
+        return flag;
+    }
+
+    /***
+     * This function perform wait operation.
+     * @param WaitInSecond Optional Timeout (default - 1 Sec)
+     * @return True/False
+     * @author Varsh Singh
+     * @Since 18/Aug/2021
+     */
+    public boolean Threadwait(int... WaitInSecond){
+        boolean flag=false;
+        int Timeout=1000;
+        if(WaitInSecond.length!=0){
+            Timeout=Timeout*WaitInSecond[0];
+        }
+        try{
+            Thread.sleep(Timeout);
+        }catch(Exception ex){
+            flag=false;
+        }
+        return flag;
     }
 
 }
--- src/test/java/TestScripts/RegisterNewUserTest.java
+++ src/test/java/TestScripts/RegisterNewUserTest.java
@@ -30,14 +30,27 @@ public class RegisterNewUserTest extends TestBase {
     boolean blnStep = false;
     private String strDescription = "";
     Random random = new Random();
-    String strCreateEmail = "Auto"+random.nextInt(0x186a0)+"@gmail.com";
-    String strPassword = "Vaanya@03";
 
     /**
-     //############################################################
+     //######################################################################################################
      //# Test Data:  TestData can be added in dicTestData.put(k,v)
-     //############################################################
+     //#######################################################################################################
      */
+    String strPreFix = "Mr.";
+    String strCreateEmail = "Auto"+random.nextInt(0x186a0)+"@gmail.com";
+    String fName = "Auto";
+    String lName = "Test";
+    String strPassword = "Vaanya@03";
+    String strDays = "1",strMonth = "7",strYears = "1975";
+
+    String personalInfo = String.format("First Name: <b>%s</b><br>Last Name: <b>%s</b></br><br>Password: <b>%s</b></br> entered successfully",fName,lName,strPassword);
+    String addressInfo =  String.format("First Name: <b>%s</b><br>Last Name: <b>%s</b></br>" +
+            "<br>Company: <b>%s</b></br><br>Address1: <b>%s</b></br><br>City: <b>%s</b></br> " +
+            "entered successfully",fName,lName,"Coforge","Punta Gorda, FL 33955, United States","Charlotte");
+    String strZip = "33948";
+    /** #################################################################################################### */
+
+
     //Test Methode for valid credentials
     @Test(description = "Login with valid credentials")
     public void RegisterWithValid(ITestContext testContext) {
@@ -62,12 +75,87 @@ public class RegisterNewUserTest extends TestBase {
         } else {
             reportStep(testContext, "FAIL", "<b>SignIn</b> link not clicked and error is as - ");
         }
-
+        // Step# 5: Enter Email Add to Register
         blnStep = regiUserPage.enterEmailCreateAcc(strCreateEmail);
         if (blnStep) {
-            reportStep(testContext, "PASS", "<b>SignIn</b> link clicked");
+            reportStep(testContext, "PASS", "Email <b> "+strCreateEmail+"</b> Entered clicked on Register");
         } else {
             reportStep(testContext, "FAIL", regiUserPage.ErrDescription);
         }
+
+        // Step# 6: Select Prefix
+        blnStep = regiUserPage.selectPrefix(strPreFix);
+        if (blnStep) {
+            reportStep(testContext, "PASS", "Prefix <b> "+strPreFix+"</b> Selected");
+        } else {
+            reportStep(testContext, "FAIL", regiUserPage.ErrDescription);
+        }
+
+        // Step# 7: Enter Personal Information
+        blnStep = regiUserPage.fillPersonalInfo(fName,lName,strPassword);
+        if (blnStep) {
+            reportStep(testContext, "PASS",personalInfo);
+        } else {
+            reportStep(testContext, "FAIL", regiUserPage.ErrDescription);
+        }
+
+        // Step# 8: Enter Personal Information DOB
+        blnStep = regiUserPage.selectDOB(strDays,strMonth,strYears);
+        if (blnStep) {
+            reportStep(testContext, "PASS",String.format("Date Of Birth <b>%s-%s-%s</b> entered",strDays,strMonth,strYears));
+        } else {
+            reportStep(testContext, "FAIL", regiUserPage.ErrDescription);
+        }
+
+        // Step# 9: Enter Personal Information Address
+        blnStep = regiUserPage.fillYourAddress(fName,lName,"Coforge","Punta Gorda, FL 33955, United States","Charlotte");
+        if (blnStep) {
+            reportStep(testContext, "PASS",addressInfo);
+        } else {
+            reportStep(testContext, "FAIL", regiUserPage.ErrDescription);
+        }
+
+        // Step# 10: Enter Personal Information Address
+        blnStep = regiUserPage.fillYourAddress(fName,lName,"Coforge","Punta Gorda, FL 33955, United States","Charlotte");
+        if (blnStep) {
+            reportStep(testContext, "PASS",addressInfo);
+        } else {
+            reportStep(testContext, "FAIL", regiUserPage.ErrDescription);
+        }
+
+        // Step# 11: Enter Personal Information Zip code
+        blnStep = regiUserPage.enterZipCode(strZip);
+        if (blnStep) {
+            reportStep(testContext, "PASS","Zip Code: <b>"+strZip+"</b> entered successfully");
+        } else {
+            reportStep(testContext, "FAIL", regiUserPage.ErrDescription);
+        }
+
+        // Step# 12: Enter Personal Information Phone numbers
+        blnStep = regiUserPage.fillMobileDetails("9412351035","9412351035","Punta Gorda, FL 33955, United States");
+        if (blnStep) {
+            reportStep(testContext, "PASS","Zip Code: <b>"+strZip+"</b> entered successfully");
+        } else {
+            reportStep(testContext, "FAIL", regiUserPage.ErrDescription);
+        }
+
+        // Step# 13: Click on Register button
+        blnStep = regiUserPage.clickRegister();
+        if (blnStep) {
+            reportStep(testContext, "PASS","Register button clicked successfully");
+        } else {
+            reportStep(testContext, "FAIL", regiUserPage.ErrDescription);
+        }
+
+        // Step# 14: Click on logout
+        blnStep = regiUserPage.logOutApp();
+        if (blnStep) {
+            reportStep(testContext, "PASS","Logout clicked successfully");
+        } else {
+            reportStep(testContext, "FAIL", regiUserPage.ErrDescription);
+        }
+
+
+
     }
 }
